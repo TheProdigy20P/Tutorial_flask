97ce4a54d7a1e3512565a1033feacf516072229e
Benoit Bourgault <benoitbourgault@outlook.com>
2018-04-25 13:33:10 -0700

    Forgot to add the import for the render_template function also the templates folder needs to be inside the app

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': "Ruy"}
+    user = {'username': 'Ruy'}
+    print(render_template('index.html', title='Home', user=user))
     return render_template('index.html', title='Home', user=user)
